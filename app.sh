b2e6490e84e6cb2eb120aa4315509179c33199cf
xajibdas-beep <xajibdas@gmail.com>
2026-09-10 02:38:55 +0600

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# ১. আপনার ইউটিউব লাইভ Stream Key-টি নিচে দিন
-STREAM_KEY="আপনার_ইউটিউব_স্ট্রিম_কি"
+# ১. আপনার আসল ইউটিউব লাইভ Stream Key
+STREAM_KEY="kavx-rgks-8ygz-gmq8-fkxs"
 
-# ২. আপনার ২২ মিনিটের ভিডিওর লিংক
-YT_VIDEO_URL="https://youtube.com"
+# ২. আপনার দেওয়া ২২ মিনিটের আসল ইউটিউব ভিডিওর লিঙ্ক
+YT_VIDEO_URL="https://youtu.be"
 
-echo "Downloading video directly from YouTube to Render Cloud..."
+echo "Downloading video directly from YouTube to GitHub Cloud Server..."
 /usr/local/bin/yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" "$YT_VIDEO_URL" -o "stream_video.mp4"
 
 echo "Starting 24/7 Unlimited Live Stream on YouTube..."
@@ -16,7 +16,7 @@ do
   -c:v libx264 -preset superfast -b:v 2000k -maxrate 2000k -bufsize 4000k \
   -c:a aac -b:a 128k -ar 44100 \
   -f flv "rtmp://://youtube.com"
-
+  
   echo "Stream crashed or disconnected. Restarting loop in 5 seconds..."
   sleep 5
 done
